af0a3b193524447219c0c7cb0418fe362a4e0b2f
Kealiwyn <67714527+Kealiwyn@users.noreply.github.com>
2021-02-09 13:59:49 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+﻿
+
+function displayChart() {
+    var table = '';
+    var rows = document.getElementById("rows").value;
+    var colmns = document.getElementById("columns").value;
+    for (var r = 1; r <= rows; r++) {
+        table += '<tr>';
+        for (var c = 1; c <= colmns; c++) {
+            table += '<td>' + 'Column: ' + c + ' Row: ' + r +  '</td>'; 
+        }
+        table += '</tr>';
+    }
+    document.getElementById("displayTable").innerHTML = '<table border=1>' + table + '</table>'
+
+}
